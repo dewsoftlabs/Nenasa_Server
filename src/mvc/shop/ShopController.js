4e47198aa02c744f2cb67d9450d5b75b89bbdf11
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-09-19 20:40:52 +0530

    ss

--- src/mvc/shop/ShopController.js
+++ src/mvc/shop/ShopController.js
@@ -98,6 +98,10 @@ const updateShop = (req, res) => {
 
 const updateLogo = (req, res) => {
 
+    if (!req.file) {
+        return res.status(400).send({ error: 'No logo file uploaded' });
+      }
+
     const filePath = req.file.filename; // Get the uploaded file filename
     console.log(req.file);
 
