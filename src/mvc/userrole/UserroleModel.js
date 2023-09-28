8c908a3a6ecf24a77e383c2f2c29d31754c0b9b5
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-09-28 21:09:17 +0530

    ss

--- src/mvc/userrole/UserroleModel.js
+++ src/mvc/userrole/UserroleModel.js
@@ -1,39 +1,69 @@
-const { connection } = require('../../../config/connection');
+const { connection } = require("../../../config/connection");
 
 const UserRoleModel = {
   getUserRoleById(userRoleId, callback) {
-    connection.query('SELECT * FROM userrole WHERE userroleid = ? AND is_delete = 0', [userRoleId], callback);
+    connection.query(
+      "SELECT * FROM userrole WHERE userroleid = ? AND is_delete = 0",
+      [userRoleId],
+      callback
+    );
   },
 
   getAllUserRoles(callback) {
-    connection.query('SELECT * FROM userrole WHERE is_delete = 0', callback);
+    connection.query("SELECT * FROM userrole WHERE is_delete = 0", callback);
   },
 
   getAllAvailableUserRoles(callback) {
-    connection.query('SELECT * FROM userrole JOIN assign_permission ON userrole.userroleid = assign_permission.userroleid JOIN permission ON permission.permission_code = assign_permission.permission_code WHERE assign_permission.is_delete = 0 GROUP BY userrole.userroleid', callback);
+    const query = `
+      SELECT *
+      FROM userrole
+        JOIN assign_permission ON userrole.userroleid = assign_permission.userroleid
+        JOIN permission ON permission.permission_code = assign_permission.permission_code
+      WHERE assign_permission.is_delete = 0
+      GROUP BY userrole.userroleid
+    `;
+
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error("Error fetching user roles:", error);
+        return callback(error, null);
+      }
+
+      callback(null, results);
+    });
   },
 
   getUserPermission(userRoleId, userid, callback) {
-    const query = 'SELECT * FROM assign_permission WHERE userroleid = ? AND is_delete = 0';
+    const query =
+      "SELECT * FROM assign_permission WHERE userroleid = ? AND is_delete = 0";
     const values = [userRoleId, userid];
 
     connection.query(query, values, callback);
   },
 
   getUserById(userid, callback) {
-    connection.query('SELECT * FROM user WHERE userid = ? AND is_delete = 0', [userid], callback);
+    connection.query(
+      "SELECT * FROM user WHERE userid = ? AND is_delete = 0",
+      [userid],
+      callback
+    );
   },
 
   getUserByname(role, callback) {
-    connection.query('SELECT * FROM userrole WHERE role = ? AND is_delete = 0', [role], callback);
+    connection.query(
+      "SELECT * FROM userrole WHERE role = ? AND is_delete = 0",
+      [role],
+      callback
+    );
   },
 
   addUserRole(userRole, callback) {
-    const trndate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const trndate = new Date().toISOString().slice(0, 19).replace("T", " ");
     const defaultValues = 0;
     const activeValues = 1;
 
-    const query = 'INSERT INTO userrole (role, trndate, status, is_delete) VALUES (?, ?, ?, ?)';
+    const query =
+      "INSERT INTO userrole (role, trndate, status, is_delete) VALUES (?, ?, ?, ?)";
     const values = [userRole.role, trndate, activeValues, defaultValues];
 
     connection.query(query, values, (error, results) => {
@@ -49,21 +79,22 @@ const UserRoleModel = {
 
   updateUserRole(userRole, userRoleId, callback) {
     const { role, status } = userRole;
-    const query = 'UPDATE userrole SET role = ?, status = ? WHERE userroleid = ?';
+    const query =
+      "UPDATE userrole SET role = ?, status = ? WHERE userroleid = ?";
     const values = [role, status, userRoleId];
 
     connection.query(query, values, callback);
   },
 
   updateUserRoleStatus(userRoleId, status, callback) {
-    const query = 'UPDATE userrole SET status = ? WHERE userroleid = ?';
+    const query = "UPDATE userrole SET status = ? WHERE userroleid = ?";
     const values = [status, userRoleId];
 
     connection.query(query, values, callback);
   },
 
   deleteUserRole(userRoleId, value, callback) {
-    const query = 'UPDATE userrole SET is_delete = ? WHERE userroleid = ?';
+    const query = "UPDATE userrole SET is_delete = ? WHERE userroleid = ?";
     const values = [value, userRoleId];
 
     connection.query(query, values, callback);
@@ -83,15 +114,19 @@ const UserRoleModel = {
           failCount++;
           checkCompletion();
         } else {
-          UserRoleModel.getUserRoleById(userRoleId, 1, (deleteError, deleteResult) => {
-            if (deleteError) {
-              failCount++;
-            } else {
-              successCount++;
-            }
+          UserRoleModel.getUserRoleById(
+            userRoleId,
+            1,
+            (deleteError, deleteResult) => {
+              if (deleteError) {
+                failCount++;
+              } else {
+                successCount++;
+              }
 
-            checkCompletion();
-          });
+              checkCompletion();
+            }
+          );
         }
       });
     }
@@ -99,7 +134,8 @@ const UserRoleModel = {
     function checkCompletion() {
       const totalCount = userRoleIds.length;
       if (successCount + failCount === totalCount) {
-        if (typeof callback === 'function') { // Check if callback is provided and is a function
+        if (typeof callback === "function") {
+          // Check if callback is provided and is a function
           callback(null, {
             totalCount,
             successCount,
@@ -108,13 +144,10 @@ const UserRoleModel = {
         }
       }
     }
-  }
-
-  ,
-
+  },
 
   permanentDeleteUserRole(userRoleId, callback) {
-    const query = 'DELETE FROM userrole WHERE userroleid = ?';
+    const query = "DELETE FROM userrole WHERE userroleid = ?";
     const values = [userRoleId];
 
     connection.query(query, values, callback);
@@ -122,13 +155,17 @@ const UserRoleModel = {
 
   userRoleById(userRoleId) {
     return new Promise((resolve, reject) => {
-      connection.query('SELECT * FROM userrole WHERE userroleid = ?', [userRoleId], (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
+      connection.query(
+        "SELECT * FROM userrole WHERE userroleid = ?",
+        [userRoleId],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
         }
-      });
+      );
     });
   },
 };
