da98f6e0a6994f1bef9c0306e3fc0a4be77d3e31
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-10-14 23:29:17 +0530

    ss

--- src/mvc/loan/loanController.js
+++ src/mvc/loan/loanController.js
@@ -43,11 +43,27 @@ const addLoan = (req, res) => {
     res.status(statusCode).send({ error: errorMessage });
   };
 
+  // Function to handle rollback in case of failure
+  const rollback = (customer_id, deposit_acc_no, guarantor_id, loan_id, collection_id, callback) => {
+    async.parallel([
+      (cb) => { InstallementModal.deleteInstallementsByCollectionId(collection_id, cb); },
+      (cb) => { CollectionModal.deleteCollection(collection_id, cb); },
+      (cb) => { loanModel.deleteLoan(loan_id, cb); },
+      (cb) => { depositAccModel.deleteDepositAcc(deposit_acc_no, cb); },
+      (cb) => { GuarantorModel.deleteGuarantor(guarantor_id, cb); },
+      (cb) => { customerModel.deleteCustomer(customer_id, cb); }
+    ], (error, results) => {
+      if (error) {
+        console.error("Rollback failed:", error);
+      }
+      callback();
+    });
+  };
+
   if (!loan || !customer || !guarantor || !collection) {
     return handleError(500, "Failed to find necessary details for loan creation");
   }
 
-  // Function to create a new customer if not found
   const createNewCustomer = (customer, callback) => {
     customerModel.addCustomer(customer, (error, customer_id) => {
       if (error) {
@@ -62,7 +78,6 @@ const addLoan = (req, res) => {
     });
   };
 
-  // Function to create a new guarantor if not found
   const createNewGuarantor = (guarantor, callback) => {
     GuarantorModel.addGuarantor(guarantor, (error, guarantor_id) => {
       if (error) {
@@ -77,7 +92,6 @@ const addLoan = (req, res) => {
     });
   };
 
-  // Function to create a new deposit account
   const createDepositAccount = (customer_id, deposit, callback) => {
     depositAccModel.adddepositAcc(customer_id, deposit, (error, deposit_acc_no) => {
       if (error) {
@@ -92,15 +106,22 @@ const addLoan = (req, res) => {
     });
   };
 
-  // Function to create a new loan
   const createNewLoan = (customer_id, deposit_acc_no, guarantor_id, callback) => {
     loanModel.addLoan(customer_id, deposit_acc_no, guarantor_id, loan, (error, loan_id) => {
       if (error) {
-        return handleError(500, "Error fetching data from the database deposit_acc_no");
+        // Perform rollback on error
+        rollback(customer_id, deposit_acc_no, guarantor_id, loan_id, 0, () => {
+          handleError(500, "Error fetching data from the database deposit_acc_no");
+        });
+        return;
       }
 
       if (!loan_id) {
-        return handleError(404, "Failed to create Loan");
+        // Perform rollback on failure to create loan
+        rollback(customer_id, deposit_acc_no, guarantor_id, loan_id, 0, () => {
+          handleError(404, "Failed to create Loan");
+        });
+        return;
       }
 
       callback(loan_id);
@@ -110,108 +131,69 @@ const addLoan = (req, res) => {
   const createNewCollection = (loan_id, branchid, userid, callback) => {
     CollectionModal.addCollection(loan_id, branchid, userid, (error, collection_id) => {
       if (error) {
-        return handleError(500, "Error fetching data from the database collection");
+        // Perform rollback on error
+        rollback(0, 0, 0, 0, collection_id, () => {
+          handleError(500, "Error fetching data from the database collection");
+        });
+        return;
       }
 
       if (!collection_id) {
-        return handleError(404, "Failed to create Collection");
+        // Perform rollback on failure to create collection
+        rollback(0, 0, 0, 0, collection_id, () => {
+          handleError(404, "Failed to create Collection");
+        });
+        return;
       }
 
       callback(collection_id);
     });
   };
 
-  // Function to create installments
   const createInstallement = (loan_period, collection, collection_id, installments, userid, callback) => {
-
-    if (loan_period == 1) {
+    if (loan_period == 1 || loan_period == 2 || loan_period == 3) {
       let successCount = 0;
       let failCount = 0;
-  
-      collection.forEach((value) => {
-        InstallementModal.addInstallement(collection_id, value, installments, userid, (error, installement_id) => {
-          if (error) {
-            // Increment the fail count
-            failCount++;
-            handleError(500, "Error creating installment");
-          } else if (!installement_id) {
-            // Increment the fail count
-            failCount++;
-            handleError(500, "Failed to create Installment");
-          } else {
-            // Increment the success count
-            successCount++;
-          }
-  
 
-          // Check if all iterations are completed
-          if (successCount + failCount === collection.length) {
-            // Call the callback with success and fail counts
-          }
-        });
-      });
-      callback(null, { successCount });
-    }else if (loan_period == 2) {
-      let successCount = 0;
-      let failCount = 0;
-  
       collection.forEach((value) => {
-        const desc = value.startDate + ',' + value.endDate;
+        const desc = (loan_period === 1) ? value : (loan_period === 2) ? value.startDate + ',' + value.endDate : value.startDate + ',' + value.endDate + '|' + value.monthAndYear;
 
         InstallementModal.addInstallement(collection_id, desc, installments, userid, (error, installement_id) => {
           if (error) {
             // Increment the fail count
             failCount++;
-            handleError(500, "Error creating installment");
+            // Perform rollback on error
+            rollback(0, 0, 0, 0, collection_id, () => {
+              handleError(500, "Error creating installment");
+            });
           } else if (!installement_id) {
             // Increment the fail count
             failCount++;
-            handleError(500, "Failed to create Installment");
+            // Perform rollback on failure to create installment
+            rollback(0, 0, 0, 0, collection_id, () => {
+              handleError(500, "Failed to create Installment");
+            });
           } else {
             // Increment the success count
             successCount++;
           }
-  
 
           // Check if all iterations are completed
           if (successCount + failCount === collection.length) {
             // Call the callback with success and fail counts
+            if (failCount > 0) {
+              // Perform rollback on failure
+              rollback(0, 0, 0, 0, collection_id, () => {
+                callback({ successCount, failCount });
+              });
+            } else {
+              callback({ successCount, failCount });
+            }
           }
         });
       });
-      callback(null, { successCount });
-    }else if (loan_period == 3) {
-      let successCount = 0;
-      let failCount = 0;
-  
-      collection.forEach((value) => {
-        const desc = value.startDate + ',' + value.endDate + '|' + value.monthAndYear;
-
-        InstallementModal.addInstallement(collection_id, desc, installments, userid, (error, installement_id) => {
-          if (error) {
-            // Increment the fail count
-            failCount++;
-            handleError(500, "Error creating installment");
-          } else if (!installement_id) {
-            // Increment the fail count
-            failCount++;
-            handleError(500, "Failed to create Installment");
-          } else {
-            // Increment the success count
-            successCount++;
-          }
-  
-
-          // Check if all iterations are completed
-          if (successCount + failCount === collection.length) {
-            // Call the callback with success and fail counts
-          }
-        });
-      });
-      callback(null, { successCount });
     }
   };
-  
 
   CustomerModel.getCustomerBynic(customer.customer_nic, (error, customerResults) => {
     if (error) {
